a1edac2813bec6de9f5fc447c78dc468d53faf00
Ruy Garcia <rrgarciach@gmail.com>
2020-12-23 14:11:03 -0700

    MariaDB default credentials with user root

--- src/config/index.js
+++ src/config/index.js
@@ -7,8 +7,8 @@ module.exports = {
   MYSQL: {
     LOCAL: {
       host: process.env.MYSQL_HOST || 'localhost',
-      user: process.env.MYSQL_USER || '',
-      password: process.env.MYSQL_PASSWORD || '',
+      user: process.env.MYSQL_USER || 'root',
+      password: process.env.MYSQL_PASSWORD,
       database: process.env.MYSQL_DATABASE || 'cropiot',
     },
   },
