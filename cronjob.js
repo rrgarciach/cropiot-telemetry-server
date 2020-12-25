4af0eba769759bf8e7c6a1f58c3f2611d617af61
Ruy Garcia <rrgarciach@gmail.com>
2020-12-25 16:20:30 -0700

    Added restarting timeout

--- cronjob.js
+++ cronjob.js
@@ -1,12 +1,12 @@
 const {exec} = require('child_process');
 
-const SECONDS_TO_RESTART = 10;
+const SECONDS_TO_RESTART = process.env.SECONDS_TO_RESTART || 3600;
 
 triggerTimeout();
 
 function checkForChanges() {
-  exec('git pull', (error, stdout, stderr) => {
-    triggerTimeout();
+  console.info('CRON_JOB: Updating code and restarting all processes...');
+  exec('git pull && pm2 restart all', (error, stdout, stderr) => {
     if (error) return console.log(`error: ${error.message}`);
     if (stderr) return console.log(`stderr: ${stderr}`);
     console.log(`stdout: ${stdout}`);
