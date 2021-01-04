d1f51d76310d4e3e58064b51d5f5063384be3c5d
Ruy Garcia <rrgarciach@gmail.com>
2021-01-04 11:13:34 -0700

    Separated restart from trigger timeout

--- cronjob.js
+++ cronjob.js
@@ -5,15 +5,25 @@ const SECONDS_TO_RESTART = process.env.SECONDS_TO_RESTART || 3600;
 triggerTimeout();
 
 function checkForChanges() {
-  console.info('CRON_JOB: Updating code and restarting all processes...');
-  exec('git pull && pm2 restart all', (error, stdout, stderr) => {
+  console.info('CRON_JOB: Updating code...');
+  exec('git pull', (error, stdout, stderr) => {
     if (error) return console.log(`error: ${error.message}`);
     if (stderr) return console.log(`stderr: ${stderr}`);
     console.log(`stdout: ${stdout}`);
+    if (stdout.includes('Unpacking objects:')) restartProcesses();
   });
 }
 
 function triggerTimeout() {
   const timeout = SECONDS_TO_RESTART * 1000;
   setTimeout(checkForChanges, timeout);
+}
+
+function restartProcesses() {
+  console.info('CRON_JOB: Restarting all processes...');
+  exec('pm2 restart all', (error, stdout, stderr) => {
+    if (error) return console.log(`error: ${error.message}`);
+    if (stderr) return console.log(`stderr: ${stderr}`);
+    console.log(`stdout: ${stdout}`);
+  });
 }
